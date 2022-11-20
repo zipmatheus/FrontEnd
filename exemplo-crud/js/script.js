609227c3c2ac99761f301b9d2cef5f677bc15546
matheus barbosa <zipmatheus@hotmail.com>
2022-11-20 15:13:26 -0300

    Melhoria com BootStrap

---
+++ exemplo-crud/js/script.js
@@ -0,0 +1,75 @@
+// request via JavaScript ajax 4 passos
+// 01 criar a váriavel
+xhttp = new XMLHttpRequest();
+var lista;
+var api = "https://zipmatheus.herokuapp.com/api/usuario/";
+
+function listar() {
+    // 02 definição do nosso request (forma e endereço)
+    xhttp.open("GET", api);
+    // 03 manda de fato a request
+    xhttp.send();
+    // 04 execução quando tiver a devolutiva do request
+    xhttp.onload = function () {
+        lista = this.responseText;
+        // console.log(lista);
+        lista = JSON.parse(lista);
+        // console.log(lista);
+        texto = "";
+        i = 0;
+        for (const u of lista) {
+            texto += `<tr onclick='editar(${i})'><td>${u.nome}</td><td>${u.email}</td></tr>`;
+            i++;
+        }
+        document.getElementById('lista').innerHTML = texto;
+    }
+}
+
+function editar(i) {
+    u = lista[i];
+    document.getElementById("nome").value = u.nome;
+    document.getElementById("email").value = u.email;
+    document.getElementById("id").value = u.id;
+}
+
+function gravar() {
+    //alert("Estamos dentro da function incluir");
+    var usuario = {};
+    usuario.nome = document.getElementById("nome").value;
+    usuario.email = document.getElementById("email").value;
+    // console.log(usuario);
+
+    usuario.id = document.getElementById("id").value;
+    if (usuario.id > 0) {
+        acao = "PUT"; // alteração
+    } else {
+        acao = "POST"; // incluir
+    }
+
+    xhttp.open(acao, api);
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.send(JSON.stringify(usuario));
+    xhttp.onload = function () {
+        // console.log(this.responseText);
+        listar();
+        limpar();
+    }
+}
+
+function limpar() {
+    document.getElementById("nome").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("id").value = "";
+}
+
+function apagar() {
+    id = document.getElementById("id").value;
+    xhttp.open("DELETE", api + id);
+    xhttp.send();
+    xhttp.onload = function () {
+        alert(this.responseText);
+        listar();
+        limpar();
+    }
+}
+listar();
