bc39450aa67fed0f496b77d65ba20ab84e3f261d
matheus barbosa <zipmatheus@hotmail.com>
2022-12-18 19:57:58 -0300

    mudanca nas urls

--- fake-api/produtos.js
+++ fake-api/produtos.js
@@ -1,8 +1,8 @@
 [
-    {"nome":"Coxinha Grande/Unidade","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/prj-loja-02/js/img/cox1.jpg","valor":6.0},
-    {"nome":"Empada Grande/Unidade","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/prj-loja-02/js/img/emp1.jpg","valor":6.5},
-    {"nome":"Quibe Grande/Unidade","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/prj-loja-02/js/img/quib1.jpg","valor":6.0},
-    {"nome":"Coxinha pequena/Cento","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/prj-loja-02/js/img/cox2.jpg","valor":40.0},
-    {"nome":"Quibe pequeno/Cento","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/prj-loja-02/js/img/emp2.jpg","valor":45.0}
-    {"nome":"Empada pequena/Cento","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/prj-loja-02/js/img/quib2.jpg","valor":40.0}
+    {"nome":"Coxinha Grande/Unidade","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/fake-api/img/cox1.jpg","valor":6.0},
+    {"nome":"Empada Grande/Unidade","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/fake-api/img/emp1.jpg","valor":6.5},
+    {"nome":"Quibe Grande/Unidade","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/fake-api/img/quib1.jpg","valor":6.0},
+    {"nome":"Coxinha pequena/Cento","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/fake-api/img/cox2.jpg","valor":40.0},
+    {"nome":"Quibe pequeno/Cento","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/fake-api/img/quib2.jpg","valor":45.0}
+    {"nome":"Empada pequena/Cento","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/fake-api/img/emp2.jpg","valor":40.0}
 ]
