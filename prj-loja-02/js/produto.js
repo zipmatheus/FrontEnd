9b6694de75dca173f906eb2ff510ac975a2b5217
matheus barbosa <zipmatheus@hotmail.com>
2022-12-18 19:29:47 -0300

    Subida projeto do curso senai

---
+++ prj-loja-02/js/produto.js
@@ -0,0 +1,8 @@
+[
+    {"nome":"Coxinha Grande/Unidade","img":"https://zipmatheus.github.io/FrontEnd/fake-api/img/chocolate-oreo.webp","valor":41.6},
+    {"nome":"Empada Grande/Unidade","img":"https://zipmatheus.github.io/FrontEnd/fake-api/img/brigadeiro-limao.webp","valor":42.5},
+    {"nome":"Quibe Grande/Unidade","img":"https://zipmatheus.github.io/FrontEnd/fake-api/img/brigadeiro-maracuja.webp","valor":30.8},
+    {"nome":"Coxinha pequena/Cento","img":"https://zipmatheus.github.io/FrontEnd/fake-api/img/brigadeiro-beijinho.webp","valor":40.5},
+    {"nome":"Quibe pequeno/Cento","img":"https://zipmatheus.github.io/FrontEnd/fake-api/img/nutela.webp","valor":45.0}
+    {"nome":"Empada pequena/Cento","img":"https://zipmatheus.github.io/FrontEnd/fake-api/img/nutela.webp","valor":45.0}
+]
