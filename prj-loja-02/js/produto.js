5c3fe3ba33fbaae84938de8cd4b5578e47ca53e3
matheus barbosa <zipmatheus@hotmail.com>
2022-12-18 19:31:09 -0300

    ajuste de pastas

--- prj-loja-02/js/produto.js
+++ prj-loja-02/js/produto.js
@@ -1,5 +1,5 @@
 [
-    {"nome":"Coxinha Grande/Unidade","img":"https://zipmatheus.github.io/FrontEnd/fake-api/img/chocolate-oreo.webp","valor":41.6},
+    {"nome":"Coxinha Grande/Unidade","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/prj-loja-02/js/cox1.jpg","valor":41.6},
     {"nome":"Empada Grande/Unidade","img":"https://zipmatheus.github.io/FrontEnd/fake-api/img/brigadeiro-limao.webp","valor":42.5},
     {"nome":"Quibe Grande/Unidade","img":"https://zipmatheus.github.io/FrontEnd/fake-api/img/brigadeiro-maracuja.webp","valor":30.8},
     {"nome":"Coxinha pequena/Cento","img":"https://zipmatheus.github.io/FrontEnd/fake-api/img/brigadeiro-beijinho.webp","valor":40.5},
