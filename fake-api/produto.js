616ca65d47170a36c4e00f3f446cd977bf0991f2
matheus barbosa <zipmatheus@hotmail.com>
2022-12-18 20:22:24 -0300

    ajuste de link

--- fake-api/produto.js
+++ fake-api/produto.js
@@ -1,7 +1,8 @@
 [
-    {"nome":"Chocolate branco com Oreo","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/fake-api/img/chocolate-oreo.webp","valor":41.6},
-    {"nome":"Brownie com brigadeiro de limão","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/fake-api/img/brigadeiro-limao.webp","valor":42.5},
-    {"nome":"Brigadeiro ao leite com maracujá","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/fake-api/img/brigadeiro-maracuja.webp","valor":30.8},
-    {"nome":"Brigadeiro ao leite com beijinho","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/fake-api/img/brigadeiro-beijinho.webp","valor":40.5},
-    {"nome":"Leite Ninho com Nutela","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/fake-api/img/nutela.webp","valor":45.0}
+    {"nome":"Coxinha Grande/Unidade","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/fake-api/img/cox1.jpg","valor":6.0},
+    {"nome":"Empada Grande/Unidade","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/fake-api/img/emp1.jpg","valor":6.5},
+    {"nome":"Quibe Grande/Unidade","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/fake-api/img/quib1.jpg","valor":6.0},
+    {"nome":"Coxinha pequena/Cento","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/fake-api/img/cox2.jpg","valor":40.0},
+    {"nome":"Quibe pequeno/Cento","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/fake-api/img/quib2.jpg","valor":45.0}
+    {"nome":"Empada pequena/Cento","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/fake-api/img/emp2.jpg","valor":40.0}
 ]
