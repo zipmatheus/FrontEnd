b4282437fac09f857b4a4e2dd72b21a510a34515
matheus barbosa <zipmatheus@hotmail.com>
2022-12-18 19:04:41 -0300

    ajuste de URLs

--- fake-api/produto.js
+++ fake-api/produto.js
@@ -1,7 +1,7 @@
 [
-    {"nome":"Chocolate branco com Oreo","img":"https://github.com/zipmatheus/FrontEnd/blob/main/fake-api/img/chocolate-oreo.webp","valor":41.6},
-    {"nome":"Brownie com brigadeiro de limão","img":"https://github.com/zipmatheus/FrontEnd/blob/main/fake-api/img/brigadeiro-limao.webp","valor":42.5},
-    {"nome":"Brigadeiro ao leite com maracujá","img":"https://github.com/zipmatheus/FrontEnd/blob/main/fake-api/img/brigadeiro-maracuja.webp","valor":30.8},
-    {"nome":"Brigadeiro ao leite com beijinho","img":"https://github.com/zipmatheus/FrontEnd/blob/main/fake-api/img/brigadeiro-beijinho.webp","valor":40.5},
-    {"nome":"Leite Ninho com Nutela","img":"https://github.com/zipmatheus/FrontEnd/blob/main/fake-api/img/nutela.webp","valor":45.0}
+    {"nome":"Chocolate branco com Oreo","img":"https://zipmatheus.github.io/FrontEnd/fake-api/img/chocolate-oreo.webp","valor":41.6},
+    {"nome":"Brownie com brigadeiro de limão","img":"https://zipmatheus.github.io/FrontEnd/fake-api/img/brigadeiro-limao.webp","valor":42.5},
+    {"nome":"Brigadeiro ao leite com maracujá","img":"https://zipmatheus.github.io/FrontEnd/fake-api/img/brigadeiro-maracuja.webp","valor":30.8},
+    {"nome":"Brigadeiro ao leite com beijinho","img":"https://zipmatheus.github.io/FrontEnd/fake-api/img/brigadeiro-beijinho.webp","valor":40.5},
+    {"nome":"Leite Ninho com Nutela","img":"https://zipmatheus.github.io/FrontEnd/fake-api/img/nutela.webp","valor":45.0}
 ]
