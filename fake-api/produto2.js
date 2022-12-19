704ab3414d02bc515209cd3a7c470f3518dc4fed
matheus barbosa <zipmatheus@hotmail.com>
2022-12-19 09:52:42 -0300

    ajuste

--- fake-api/produto2.js
+++ fake-api/produto2.js
@@ -4,5 +4,5 @@
     {"nome":"Quibe Grande/Unidade","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/fake-api/img/quib1.jpg","valor":6.0},
     {"nome":"Coxinha pequena/Cento","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/fake-api/img/cox2.jpg","valor":40.0},
     {"nome":"Quibe pequeno/Cento","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/fake-api/img/quib2.jpg","valor":45.0}
-    {"nome":"Empada pequena/Cento","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/fake-api/img/emp2.jpg","valor":40.0}
+    //{"nome":"Empada pequena/Cento","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/fake-api/img/emp2.jpg","valor":40.0}
 ]
