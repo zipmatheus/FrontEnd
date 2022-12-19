eb49b2afe5b5cf2dfd78e3a70b7fbe7838d67f11
matheus barbosa <zipmatheus@hotmail.com>
2022-12-19 09:35:14 -0300

    ajuste

---
+++ fake-api/produto2.js
@@ -0,0 +1,8 @@
+[
+    {"nome":"Coxinha Grande/Unidade","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/fake-api/img/cox1.jpg","valor":6.0},
+    {"nome":"Empada Grande/Unidade","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/fake-api/img/emp1.jpg","valor":6.5},
+    {"nome":"Quibe Grande/Unidade","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/fake-api/img/quib1.jpg","valor":6.0},
+    {"nome":"Coxinha pequena/Cento","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/fake-api/img/cox2.jpg","valor":40.0},
+    {"nome":"Quibe pequeno/Cento","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/fake-api/img/quib2.jpg","valor":45.0}
+    {"nome":"Empada pequena/Cento","img":"https://raw.githubusercontent.com/zipmatheus/FrontEnd/main/fake-api/img/emp2.jpg","valor":40.0}
+]
